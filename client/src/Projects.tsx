db170b465d71b9ef06494775391af0e66a918003
Tyler Stahl <tstahl488@gmail.com>
2023-12-15 19:57:52 +0000

    tailwind project card

--- client/src/Projects.tsx
+++ client/src/Projects.tsx
@@ -1,3 +1,10 @@
 export function Projects() {
-  return <div>projects</div>;
+  return (
+    <div className="border-2 rounded w-3/4">
+      <h1>name</h1>
+      <img src="imageUrl" alt=" project name" />
+      <h2>Github</h2>
+      <h2>Domain</h2>
+    </div>
+  );
 }
